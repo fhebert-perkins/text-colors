8cca07fa8a79e5a592088dc330d9dc9e6cb11d63
finley hebert-perkins <finley.hebert@gmail.com>
2014-10-23 13:04:06 -0700

    initial commit

---
+++ unicode.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+@app.route("/", methods=["POST","GET"])
+def derp():
+	if request.method == "POST":
+		string = request.form["string"]
+	else:
+		string = ""
+	divs = []
+	for i in range(len(string.encode("hex"))/5):
+		divs.append(string.encode("hex")[(i*5):(i*5)+5]+"0")
+
+	return render_template("derp.html", divs=divs)
+if __name__ == "__main__":
+	app.run(debug=True, port=8080, host="0.0.0.0")
